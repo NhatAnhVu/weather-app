cb544317cf0454037d7ac4b10c69f816347bc4f7
Vu Nhat Anh <anhvn@devcamp.edu.vn>
2023-03-09 16:09:38 +0700

    forecast

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ function App() {
 
   const handleEnter = async (e) => {
     if (e.key === "Enter") {
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=08aa986ea4e4b5fd2c0535dd822babbd`)
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=08aa986ea4e4b5fd2c0535dd822babbd`)
       const data = await res.json()
       setResult(data)
       setCityInput("")
     }
   };
+
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  const today = new Date()
+  const getDayName = (paramN) => {
+    if (today.getDay() + paramN > 6) {
+      return days[(today.getDay()+paramN) % 6]
+    }
+    return days[today.getDay()+paramN]
+  }
 
   return (
     <div className="App">
@@ -27,12 +36,34 @@ function App() {
           />
           <div className="container result-container">
             <div className="display">
-              <img src={`./svgtopng/${result.weather[0].icon}.png`} alt="" className="display-info-image" />
+              <img src={`./svgtopng/${result.list[1].weather[0].icon}.png`} alt="" className="display-info-image" />
               <div className="info">
                 <span>Today</span>
-                <h1>{result.name}</h1>
-                <span>{`Temperature: ${result.main.temp}℃`}</span>
-                <span>{result.weather[0].description}</span>
+                <h1>{result.city.name}</h1>
+                <span>{`Temperature: ${result.list[1].main.temp}°F`}</span>
+                <span>{result.list[1].weather[0].description}</span>
+              </div>
+            </div>
+            <div className="forecast">
+              <div className="forecast-day">
+                <strong>{getDayName(1)}</strong>
+                <img src={`./svgtopng/${result.list[9].weather[0].icon}.png`} alt="" />
+                <span>{`${result.list[8].main.temp}°F`}</span>
+              </div>
+              <div className="forecast-day">
+                <strong>{getDayName(2)}</strong>
+                <img src={`./svgtopng/${result.list[17].weather[0].icon}.png`} alt="" />
+                <span>{`${result.list[17].main.temp}°F`}</span>
+              </div>
+              <div className="forecast-day">
+                <strong>{getDayName(3)}</strong>
+                <img src={`./svgtopng/${result.list[25].weather[0].icon}.png`} alt="" />
+                <span>{`${result.list[25].main.temp}°F`}</span>
+              </div>
+              <div className="forecast-day">
+                <strong>{getDayName(4)}</strong>
+                <img src={`./svgtopng/${result.list[33].weather[0].icon}.png`} alt="" />
+                <span>{`${result.list[33].main.temp}°F`}</span>
               </div>
             </div>
           </div>
